9a87ee8bea70d6af3c3240ede23532d64ecbbd37
harshavardhanp <harshavardhan.puppala@accoliteindia.com>
2021-08-17 19:34:46 +0530

    toggling the cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
+
+import { uiSliceActions } from "../../store/ui-slice";
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartHandler = () => {
+    dispatch(uiSliceActions.toggleCart());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
